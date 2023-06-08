a2621ff5ed0764732e78d71887095e72fe1b8c49
firedragonal <79261530+firedragonal@users.noreply.github.com>
2023-06-08 13:48:05 +0200

    Add files via upload

---
+++ C00__00/ex05/ft_print_comb.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alphan <alphan@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/07 15:21:27 by alphan            #+#    #+#             */
+/*   Updated: 2023/06/08 12:00:50 by alphan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	writee(char a, char b, char c)
+{
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+}
+
+void	boucle(char a, char b, char c)
+{
+	while (a <= '6')
+	{
+		if (c <= '9')
+		{
+			writee(a, b, c);
+			write(1, ",", 1);
+			c++;
+		}
+		if (c > '9')
+		{
+			b++;
+			c = b + 1;
+		}
+		if (b > '9')
+		{
+			a++;
+			b = a + 1;
+			c = b + 1;
+		}
+		if (a == '7' && b == '8' && c == '9')
+		{
+			writee(a, b, c);
+		}
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0';
+	b = '1';
+	c = '2';
+	boucle(a, b, c);
+}
